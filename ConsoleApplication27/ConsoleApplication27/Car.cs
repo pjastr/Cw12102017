02ef6e32cc57cfc74e0baff4b2e43cd67126aa51
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-12 18:58:32 +0200

    Add files via upload

---
+++ ConsoleApplication27/ConsoleApplication27/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Car:Interface1
+    {
+        internal string marka;
+        private int rokProdukcji;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        internal double srednieSpalanie;
+
+        public void UstawRokProdukcji(int rok)
+        {
+            this.rokProdukcji = rok;
+        }
+
+        public string Info()
+        {
+            return marka + " " + rokProdukcji;
+        }
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            return srednieSpalanie * dlugoscTrasy;
+        }
+
+        public double ObliczKosztSpalania(double dlugoscTrasy, double cenaPaliwa)
+        {
+            return ObliczSpalanie(dlugoscTrasy) * cenaPaliwa;
+        }
+    }
+
+
+}
---
+++ ConsoleApplication27/ConsoleApplication27/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.marka = "Skoda";
+            car1.UstawRokProdukcji(2012);
+            car1.srednieSpalanie = 3;
+            Console.WriteLine(car1.ObliczKosztSpalania(200, 5));
+
+            //Car car2 = new Car();
+            //car2.marka = "Ford";
+            //car2.UstawRokProdukcji(2015);
+
+            //Console.WriteLine(car2.Info());
+
+            //car2 = car1;
+
+            Console.ReadKey();
+
+        }
+    }
+
+    
+}
