8b59ac0fb64daa9e105e5fd301407b88b1aac11a
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-12 19:00:42 +0200

    Add files via upload

---
+++ ConsoleApplication29/ConsoleApplication29/Koszyk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication29
+{
+    class Koszyk
+    {
+        public List<Produkt> lista = new List<Produkt>();
+
+        public void DodajeProdukt(string nazwaP, int cenaP)
+        {
+            Produkt produktTemp = new Produkt();
+            produktTemp.cena = cenaP;
+            produktTemp.nazwa = nazwaP;
+            lista.Add(produktTemp);
+            Console.WriteLine("dodano produkt: {0}, cena: {1}", nazwaP, cenaP);
+        }
+
+        public void Ilosc()
+        {
+            Console.WriteLine(lista.Count);
+        }
+
+        public void Suma()
+        {
+            double suma = 0;
+            foreach(var element in lista)
+            {
+                suma += element.cena;
+            }
+            Console.WriteLine(suma);
+        }
+    }
+}
---
+++ ConsoleApplication29/ConsoleApplication29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Koszyk koszykJana = new Koszyk();
+
+            koszykJana.DodajeProdukt("Jabłko", 3);
+            koszykJana.DodajeProdukt("Chleb", 3);
+            koszykJana.DodajeProdukt("Mleko", 2);
+            koszykJana.DodajeProdukt("Sok", 4);
+            koszykJana.Ilosc();
+            koszykJana.Suma();
+            Console.ReadKey();
+        }
+    }
+}
