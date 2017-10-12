fec0cdd883a817ef3080ba92b51eaa1df78de96e
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-12 18:59:21 +0200

    Add files via upload

---
+++ ConsoleApplication28/ConsoleApplication28/Osoba.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication28
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int rokUrodzenia;
+        public int waga;
+        public int wzrost;
+        public bool okulary;
+        public string plec;
+
+        public int ObliczWiek()
+        {
+            return DateTime.Now.Year - rokUrodzenia;
+        }
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine("Dane osoby: {0} {1}", imie, nazwisko);
+            Console.WriteLine("Wiek: {0}", ObliczWiek());
+        }
+        public void Plec()
+        {
+            if (plec == "Kobieta")
+            {
+                Console.WriteLine("Dyrektorem jest Pani {0} {1}", imie, nazwisko);
+            }
+            else
+            {
+                Console.WriteLine("Dyrektorem jest Pan {0} {1}", imie, nazwisko);
+            }
+        }
+    }
+}
---
+++ ConsoleApplication28/ConsoleApplication28/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "Jan";
+            dyrektor.nazwisko = "Nowak";
+            dyrektor.rokUrodzenia = 1980;
+            
+            dyrektor.WyswietlInformacje();
+            dyrektor.plec = "Mężczyzna";
+            dyrektor.Plec();
+
+            Console.ReadKey();
+        }
+    }
+}
